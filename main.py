16879a8af9e6101a88182f89aa79384945b50a54
pasquale <apcipollone3@gmail.com>
2019-09-16 22:41:29 -0700

    Sensor wired.  Able to read, next to write

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+import board
+import busio
+import adafruit_adxl34x
+i2c = busio.I2C(board.SCL, board.SDA)
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+#1 side//side
+
+while True:
+        print("%f"%accelerometer.acceleration[0])
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(18,GPIO.OUT)
+        if accelerometer.acceleration[0] > 1:
+            print ("led on")
+            GPIO.output(18,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(18,GPIO.LOW)
+
+
