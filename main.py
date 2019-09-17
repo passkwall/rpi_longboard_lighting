16677a1012c8adb90ea5d8317bf0d08a2248e349
pasquale <apcipollone3@gmail.com>
2019-09-16 22:45:18 -0700

    pretty / todo

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import RPi.GPIO as GPIO
 import time
+import adafruit_adxl34x
 import board
 import busio
-import adafruit_adxl34x
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 #1 side//side
+#0 forward/backward
 
 while True:
         print("%f"%accelerometer.acceleration[0])
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(18,GPIO.OUT)
+
         if accelerometer.acceleration[0] > 1:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(18,GPIO.OUT)
             print ("led on")
             GPIO.output(18,GPIO.HIGH)
             time.sleep(1)
             GPIO.output(18,GPIO.LOW)
 
 
+## TODO - wire a button to begin recording to csv file.
